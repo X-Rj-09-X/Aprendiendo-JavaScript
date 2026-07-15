1db1b6722222f8ed0681caf208f59b4d18540905
X-Rj-09-X <rikardojfp@hotmail.com>
2026-07-15 16:38:23 -0500

    funciones con parametros

--- Funciones.js
+++ Funciones.js
@@ -1,10 +1,18 @@
-// definicion o declaracion
+/*// definicion o declaracion
 function saludar(){
     console.log("HOLA!!!");
 }
 
 //llamar una funcion.
-saludar();
-saludar();
-saludar();
-saludar();
+saludar();*/
+
+//declaramos funcion con parametros
+function sumar(num1, num2){
+    let resultado = num1 + num2;
+    console.log("El resultado es:" + resultado);
+}
+
+//llamar funcion
+sumar(14, 20);
+sumar(9, 8);
+sumar(70, 35);
